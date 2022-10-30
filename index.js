215e055e6e79c58427656604c552a22023f8ad67
diego <guatemaladiegojose@gmail.com>
2022-10-30 13:34:40 -0400

    commit tree fcd389c1e4e60b6af3e5ee851f89a83518bd5bf9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+// returns a year the Denver Broncos Won the Superbowl
+
+//array.prototype.find()
+
+//record.result.find()
+
+
+function callback(record) {
+  console.log("record", record)
+  return record.result === "W"
+}
+
+
+function superbowlWin(record) {
+  let found = record.find(callback)
+  if (found){
+    return found.year
+  }
+  
+  
+  console.log("found",found)
+}  
+
+
+
+
+
+
